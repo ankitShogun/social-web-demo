9ec1c8c284bfc57b6d08914b81bfed811860ead2
Ankit Brahmbhatt <ankitbrahm.bhatt@eminds.ai>
2024-05-29 12:11:54 +0530

    added parent urls

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -4,4 +4,7 @@ export const parentURLs = [
   "https://experience-guide.becknprotocol.io",
   "https://experience.becknprotocol.io",
   "http://localhost:3001",
+  "https://experience-dev.becknprotocol.io",
+  "https://experience-staging.becknprotocol.io",
+  "https://experience-prod.becknprotocol.io",
 ];
